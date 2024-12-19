727055b337b7f5a1ccf19e6e31464a82cbfaa9bb
Hijithan Arunthavarajah <zjac082@live.rhul.ac.uk>
2024-12-19 12:58:53 +0000

    refactor: modularize css into three files
    The three files are: styles.css (global files), pc-styles.css and
    phone-styes.css.

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -4,3 +4,31 @@ const navbarMenu = document.querySelector('.navbar-menu');
 navbarToggle.addEventListener('click', () => {
     navbarMenu.classList.toggle('active');
 });
+
+const listItems = document.querySelectorAll('.navbar-menu li');
+const navbar = document.querySelector('.navbar-container');
+let wrappedSet = false;
+
+function checkTextWrapping() {
+    if (window.matchMedia('(min-width: 769px)').matches) {
+        if (navbar.clientHeight > 23 && !wrappedSet) {
+            listItems.forEach(item => {
+                item.classList.add('wrapped'); 
+            });
+            wrappedSet = true;
+        } else if (navbar.clientHeight > 38 && wrappedSet) {
+            console.log("Hello");
+        } else {
+            listItems.forEach(item => {
+                item.classList.remove('wrapped'); 
+            });
+            wrappedSet = false;
+        }
+    }
+}
+  
+  // Call the function initially
+  checkTextWrapping();
+  
+  // Add an event listener to check when window resizes
+  window.addEventListener('resize', checkTextWrapping);
