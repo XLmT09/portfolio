92ce46b10b90130497667a112f1b649c314a69ee
Hijithan Arunthavarajah <zjac082@live.rhul.ac.uk>
2024-12-19 15:35:27 +0000

    feat: add seperate navbar logic for phone and PC

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -5,30 +5,9 @@ navbarToggle.addEventListener('click', () => {
     navbarMenu.classList.toggle('active');
 });
 
-const listItems = document.querySelectorAll('.navbar-menu li');
-const navbar = document.querySelector('.navbar-container');
-let wrappedSet = false;
-
-function checkTextWrapping() {
-    if (window.matchMedia('(min-width: 769px)').matches) {
-        if (navbar.clientHeight > 23 && !wrappedSet) {
-            listItems.forEach(item => {
-                item.classList.add('wrapped'); 
-            });
-            wrappedSet = true;
-        } else if (navbar.clientHeight > 38 && wrappedSet) {
-            console.log("Hello");
-        } else {
-            listItems.forEach(item => {
-                item.classList.remove('wrapped'); 
-            });
-            wrappedSet = false;
-        }
+// Close the menu when clicking anywhere outside
+document.addEventListener('click', (event) => {
+    if (!navbarMenu.contains(event.target) && !navbarToggle.contains(event.target)) {
+        navbarMenu.classList.remove('active');
     }
-}
-  
-  // Call the function initially
-  checkTextWrapping();
-  
-  // Add an event listener to check when window resizes
-  window.addEventListener('resize', checkTextWrapping);
+});
