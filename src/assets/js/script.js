ceccc693617c8c68f7a28c2fd3a29dc376dc3c63
Hijithan Arunthavarajah <zjac082@live.rhul.ac.uk>
2024-12-23 16:40:31 +0000

    feat: add github links in the pop up

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -11,16 +11,25 @@ document.addEventListener('click', (event) => {
         navbarMenu.classList.remove('active');
     }
 });
-
-function showPopup(element) {
+function showPopup(element, showGithubIcon = false, githubLink = "") {
   const popup = document.getElementById("popup");
   const popupText = document.getElementById("popup-text");
+  const githubPopUp = document.querySelector(".github-popup-btn");
+  const githubIcon = document.querySelector(".fa-brands.fa-github");
 
   // Get the hidden content from the clicked box
   const content = element.querySelector(".hidden-content").innerHTML;
 
   // Set the content inside the popup
   popupText.innerHTML = content;
+
+  // Show or hide the GitHub icon based on the parameter
+  if (showGithubIcon) {
+    githubPopUp.style.display = "inline-block";
+    githubIcon.parentElement.href = githubLink; // Set the GitHub link if provided
+  } else {
+    githubPopUp.style.display = "none";
+  }
 
   // Show the popup with animation
   popup.style.display = "flex";
@@ -55,13 +64,3 @@ function enableScroll() {
 function preventDefault(e) {
   e.preventDefault();
 }
-
-// If a box has a link, open it in a new tab
-document.querySelectorAll('.box').forEach(box => {
-  box.addEventListener('click', () => {
-    const url = box.getAttribute('data-url');
-    if (url) {
-      window.open(url, '_blank');
-    }
-  });
-});
